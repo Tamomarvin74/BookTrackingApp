08e21721f874dbd9dd3d107d8860e4f947f67a27
Tamo Marvin <tamomarvin74@gmail.com>
2025-07-30 15:26:46 +0100

    Initial Commit

---
+++ BookTrackerApp/BookTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookTrackerAppApp.swift
+//  BookTrackerApp
+//
+//  Created by Tamo Marvin Achiri   on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct BookTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookTrackerAppTests/BookTrackerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookTrackerAppTests.swift
+//  BookTrackerAppTests
+//
+//  Created by Tamo Marvin Achiri   on 7/30/25.
+//
+
+import Testing
+@testable import BookTrackerApp
+
+struct BookTrackerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
